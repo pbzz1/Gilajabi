8c47260ad469edd1c670f23af6e8a40bd18fdc8f
mjchoi1223 <1971407@hansung.ac.kr>
2025-03-25 19:07:49 +0900

    윈도우 환경 테스트

--- lib/myhomepage.dart
+++ lib/myhomepage.dart
@@ -36,7 +36,7 @@ class MyHomePage extends StatelessWidget {
         ],
       ),
       body: const Center(
-        child: Text('홈 화면'),
+        child: Text('3월 25일 윈도우 환경 카카오 로그인 테스트 완료'),
       ),
     );
   }
