e9868b1900bd849d2a7a60b9fc3f1754f2d6abd1
mjchoi1223 <1971407@hansung.ac.kr>
2025-04-07 13:56:10 +0900

    카카오 맵

--- lib/nearby_facilities_page.dart
+++ lib/nearby_facilities_page.dart
@@ -1,21 +1,33 @@
-// lib/nearby_facilities_page.dart
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:io'; // Android WebView 초기화용
 
-class NearbyFacilitiesPage extends StatelessWidget {
+class NearbyFacilitiesPage extends StatefulWidget {
   const NearbyFacilitiesPage({super.key});
+
+  @override
+  State<NearbyFacilitiesPage> createState() => _NearbyFacilitiesPageState();
+}
+
+class _NearbyFacilitiesPageState extends State<NearbyFacilitiesPage> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) {
+      WebView.platform = SurfaceAndroidWebView();
+    }
+    _controller = WebViewController()
+      ..loadFlutterAsset('assets/html/kakao_map.html')
+      ..setJavaScriptMode(JavaScriptMode.unrestricted);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('주변 시설'),
-      ),
-      body: const Center(
-        child: Text(
-          '주변 시설 페이지',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
+      appBar: AppBar(title: const Text('주변 시설')),
+      body: WebViewWidget(controller: _controller),
     );
   }
 }
