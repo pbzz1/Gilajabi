22fccb2949d1e9ff768831fcd54619e7b1fcefd4
mjchoi1223 <1971407@hansung.ac.kr>
2025-04-07 14:47:27 +0900

    카카오 맵

--- lib/nearby_facilities_page.dart
+++ lib/nearby_facilities_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'dart:io';
 
 class NearbyFacilitiesPage extends StatefulWidget {
   const NearbyFacilitiesPage({super.key});
@@ -15,13 +14,6 @@ class _NearbyFacilitiesPageState extends State<NearbyFacilitiesPage> {
   @override
   void initState() {
     super.initState();
-
-    // ✅ Android platform 설정 (중요)
-    if (Platform.isAndroid) {
-      WebView.platform = SurfaceAndroidWebView();
-    }
-
-    // ✅ WebViewController 구성
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..loadFlutterAsset('assets/html/kakao_map.html');
