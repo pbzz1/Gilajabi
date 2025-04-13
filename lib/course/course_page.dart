c052891b01d5ed8f3952a0e38315551a2d9771f7
mjchoi1223 <1971407@hansung.ac.kr>
2025-04-14 04:09:46 +0900

    6개 코스 추가

--- lib/course/course_page.dart
+++ lib/course/course_page.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+import 'baegak_info_page.dart';
+import 'naksan_info_page.dart';
+import 'heunginjimun_info_page.dart';
+import 'namsan_info_page.dart';
+import 'sungnyemun_info_page.dart';
+import 'inwangsan_info_page.dart';
+
 class CoursePage extends StatefulWidget {
   const CoursePage({super.key});
 
   @override
-  State<CoursePage> createState() => _PostTab();
+  State<CoursePage> createState() => _CoursePageState();
 }
 
-class _PostTab extends State<CoursePage> {
+class _CoursePageState extends State<CoursePage> {
   late final WebViewController _controller;
 
   @override
@@ -17,13 +24,39 @@ class _PostTab extends State<CoursePage> {
 
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..addJavaScriptChannel(
+        'flutter_inappwebview',
+        onMessageReceived: (message) {
+          final course = message.message;
+          switch (course) {
+            case 'baegak':
+              Navigator.push(context, MaterialPageRoute(builder: (_) => const BaegakInfoPage()));
+              break;
+            case 'naksan':
+              Navigator.push(context, MaterialPageRoute(builder: (_) => const NaksanInfoPage()));
+              break;
+            case 'heunginjimun':
+              Navigator.push(context, MaterialPageRoute(builder: (_) => const HeunginjimunInfoPage()));
+              break;
+            case 'namsan':
+              Navigator.push(context, MaterialPageRoute(builder: (_) => const NamsanInfoPage()));
+              break;
+            case 'sungnyemun':
+              Navigator.push(context, MaterialPageRoute(builder: (_) => const SungnyemunInfoPage()));
+              break;
+            case 'inwangsan':
+              Navigator.push(context, MaterialPageRoute(builder: (_) => const InwangsanInfoPage()));
+              break;
+          }
+        },
+      )
       ..loadRequest(Uri.parse('https://pbzz1.github.io/Gilajabi/kakao_map.html'));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('주변 시설')),
+      appBar: AppBar(title: const Text('코스 선택')),
       body: WebViewWidget(controller: _controller),
     );
   }
